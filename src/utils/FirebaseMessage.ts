c4cd9a5ae56cd9a7921b899e61848dc80325f2af
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-03-04 20:25:39 +0700

    Implement code for Fcm-token flow.

---
+++ src/utils/FirebaseMessage.ts
@@ -0,0 +1,42 @@
+import messaging from '@react-native-firebase/messaging'
+import {useEffect} from 'react'
+import {PermissionsAndroid} from 'react-native'
+
+import {getPlatFormOS} from '@utils/Helpers'
+
+export async function requestNotificationPermission() {
+  if (getPlatFormOS() === 'android') {
+    return await requestAndroidNotificationPermission()
+  }
+  return await requestIOSNotificationPermission()
+}
+
+export async function requestAndroidNotificationPermission() {
+  const isNotificationGranted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+  )
+
+  return isNotificationGranted === PermissionsAndroid.RESULTS.GRANTED
+}
+
+export async function requestIOSNotificationPermission() {
+  const authStatus = await messaging().requestPermission()
+  const isEnabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL
+  return isEnabled
+}
+
+export async function getFCMToken() {
+  return await messaging().getToken()
+}
+
+export function subScribeForegroundFirebaseMessage(handlers: ((message: any) => void)[]) {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      handlers.forEach(handler => handler(remoteMessage))
+    })
+
+    return unsubscribe
+  }, [])
+}
