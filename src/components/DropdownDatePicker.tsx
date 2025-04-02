3fe1d0b33457e98249882d342e0aa57069328a60
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-04-02 07:01:12 +0700

    Using modal for drop down date picker instead of menu of react-native-paper.
    To avoid 1 frame render drop down at position (0, 0).
    But drop down still slowly render because date picker is heavy component.
    
    If the project finish, the slow rendering of date picker can be improve.

--- src/components/DropdownDatePicker.tsx
+++ src/components/DropdownDatePicker.tsx
@@ -1,6 +1,7 @@
 import React, {useRef, useState} from 'react'
-import {StyleSheet} from 'react-native'
-import {IconButton, Menu, Text, TouchableRipple} from 'react-native-paper'
+import {StyleSheet, View, useWindowDimensions} from 'react-native'
+import Modal from 'react-native-modal'
+import {IconButton, Text, TouchableRipple} from 'react-native-paper'
 import {DateType} from 'react-native-ui-datepicker'
 
 import DateTimePicker, {DataOnChange} from '@components/DateTimePicker'
@@ -40,6 +41,13 @@ export default function DropdownDatePicker({
   const multipleDate = useRef<DateType[]>()
   const startDate = useRef<DateType>()
   const endDate = useRef<DateType>()
+  const anchorRef = useRef<View>(null)
+  const {width: screenWidth, height: screenHeight} = useWindowDimensions()
+
+  const contentWidth = contentContainerStyle
+    ? contentContainerStyle.width
+    : styles.contentContainer.width
+  const contentHeight = contentWidth - 20
 
   function transformDate(date: Date) {
     return date.toLocaleDateString('en-GB', {
@@ -50,88 +58,125 @@ export default function DropdownDatePicker({
     })
   }
 
+  function calculateDropdownPosition() {
+    if (!anchorRef) return {top: 0, left: 0}
+
+    let top = 0
+    let left = 0
+    anchorRef.current?.measure((_x, _y, _width, height, pageX, pageY) => {
+      // width, height is anchor's width-height.
+      // pageX, pageY is position of anchor (0-screenWidth, 0-screenHeight).
+      const marginTop = 10
+      top = pageY + height + marginTop
+      left = pageX
+
+      const marginRight = 20
+      const right = pageX + contentWidth
+      if (right >= screenWidth) {
+        left = pageX - (right - screenWidth) - marginRight
+      }
+
+      const offsetY = 100
+      const marginBottom = 20
+      const bottom = pageY + height + marginBottom + contentHeight + offsetY
+      if (bottom >= screenHeight) {
+        top = pageY - height - marginBottom - contentHeight
+      }
+    })
+
+    return {top: top, left: left}
+  }
+
   return (
-    <Menu
-      elevation={0}
-      contentStyle={{...styles.contentContainer, ...contentContainerStyle}}
-      visible={visible}
-      onDismiss={() => {
-        setVisible(false)
-        setLabel(dateText.current ?? placeholder)
-      }}
-      anchorPosition='bottom'
-      anchor={
-        <TouchableRipple
-          style={{...styles.container, ...containerStyle}}
-          onPress={() => setVisible(val => !val)}>
-          <>
-            <Text
-              numberOfLines={1}
-              ellipsizeMode='tail'
-              style={{...styles.placeholder, ...placeholderStyle}}>
-              {label}
-            </Text>
-            <IconButton
-              style={styles.iconButton}
-              icon={dateText.current ? 'close' : 'calendar-month-outline'}
-              size={20}
-              iconColor={placeholderIconColor ?? theme.colors.onSurfaceVariant}
-              onPress={() => {
-                dateText.current = null
-                singleDate.current = undefined
-                multipleDate.current = undefined
-                startDate.current = undefined
-                endDate.current = undefined
-                setLabel(placeholder)
-              }}
-            />
-          </>
-        </TouchableRipple>
-      }>
-      <DateTimePicker
-        mode={mode}
-        containerHeight={
-          contentContainerStyle
-            ? contentContainerStyle.width - 20
-            : styles.contentContainer.width - 20
-        }
-        minDate={minDate}
-        maxDate={maxDate}
-        disabledDates={disabledDates}
-        initialSingleDate={singleDate.current}
-        initialMultipleDate={multipleDate.current}
-        initialStartDate={startDate.current}
-        initialEndDate={endDate.current}
-        onChange={data => {
-          if (mode === 'single') {
-            singleDate.current = data.date
-            dateText.current = data.date
-              ? transformDate(data.date as Date)
-              : null
-          }
-          if (mode === 'multiple') {
-            multipleDate.current = data.dates
-            dateText.current =
-              data.dates?.length != 0
-                ? data
-                    .dates!.map((item: DateType) => transformDate(item as Date))
-                    .join(', ')
-                : null
-          }
-          if (mode === 'range') {
-            startDate.current = data.startDate
-            endDate.current = data.endDate
-            dateText.current =
-              data.startDate && data.endDate
-                ? `${transformDate(data.startDate as Date)} - ${transformDate(
-                    data.endDate as Date,
-                  )}`
-                : null
-          }
-          onChange(data)
+    <View>
+      <TouchableRipple
+        ref={anchorRef}
+        style={{...styles.container, ...containerStyle}}
+        onPress={() => setVisible(val => !val)}>
+        <>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode='tail'
+            style={{...styles.placeholder, ...placeholderStyle}}>
+            {label}
+          </Text>
+          <IconButton
+            style={styles.iconButton}
+            icon={dateText.current ? 'close' : 'calendar-month-outline'}
+            size={20}
+            iconColor={placeholderIconColor ?? theme.colors.onSurfaceVariant}
+            onPress={() => {
+              dateText.current = null
+              singleDate.current = undefined
+              multipleDate.current = undefined
+              startDate.current = undefined
+              endDate.current = undefined
+              setLabel(placeholder)
+            }}
+          />
+        </>
+      </TouchableRipple>
+      <Modal
+        style={{
+          ...styles.contentContainer,
+          ...contentContainerStyle,
+          position: 'absolute',
+          ...calculateDropdownPosition(),
         }}
-      />
-    </Menu>
+        testID='react-native-modal'
+        isVisible={visible}
+        onBackdropPress={() => {
+          setVisible(false)
+          setLabel(dateText.current ?? placeholder)
+        }}
+        animationIn='fadeIn'
+        animationOut='fadeOut'
+        animationInTiming={1}
+        animationOutTiming={1}
+        backdropColor='transparent'>
+        <DateTimePicker
+          mode={mode}
+          containerHeight={contentHeight}
+          minDate={minDate}
+          maxDate={maxDate}
+          disabledDates={disabledDates}
+          initialSingleDate={singleDate.current}
+          initialMultipleDate={multipleDate.current}
+          initialStartDate={startDate.current}
+          initialEndDate={endDate.current}
+          onChange={data => {
+            if (mode === 'single') {
+              singleDate.current = data.date
+              dateText.current = data.date
+                ? transformDate(data.date as Date)
+                : null
+            }
+            if (mode === 'multiple') {
+              multipleDate.current = data.dates
+              dateText.current =
+                data.dates?.length != 0
+                  ? data
+                      .dates!.map((item: DateType) =>
+                        transformDate(item as Date),
+                      )
+                      .join(', ')
+                  : null
+            }
+            if (mode === 'range') {
+              startDate.current = data.startDate
+              endDate.current = data.endDate
+              dateText.current =
+                data.startDate && data.endDate
+                  ? `${transformDate(data.startDate as Date)} - ${transformDate(
+                      data.endDate as Date,
+                    )}`
+                  : null
+            }
+            onChange(data)
+          }}
+        />
+      </Modal>
+    </View>
   )
 }
 
@@ -152,8 +197,8 @@ function getStyles(theme: AdaptiveMD3Theme) {
       justifyContent: 'space-between',
     },
     contentContainer: {
-      marginTop: -10,
-      paddingTop: -10,
+      margin: 0,
+      padding: 0,
       width: 300,
       borderWidth: 1,
       borderRadius: 5,
