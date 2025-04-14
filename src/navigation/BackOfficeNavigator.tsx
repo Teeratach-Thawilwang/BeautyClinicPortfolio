b6a29531df00ac7a1895d0f5ec74adea8b02ae78
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-04-14 08:17:12 +0700

    Refactor screenOptions, Add course detail, create screen, add param backBehavior='history' to enable go back to previous screen but when need to use push method when navigate.

--- src/navigation/BackOfficeNavigator.tsx
+++ src/navigation/BackOfficeNavigator.tsx
@@ -1,10 +1,16 @@
-import {createDrawerNavigator} from '@react-navigation/drawer'
-import {NavigatorScreenParams} from '@react-navigation/native'
+import {
+  DrawerNavigationOptions,
+  createDrawerNavigator,
+} from '@react-navigation/drawer'
+import {NavigatorScreenParams, RouteProp} from '@react-navigation/native'
 import React from 'react'
 
 import {useTheme} from '@context-providers/ThemeProvider'
 import {useResponsiveScreen} from '@hooks/CommonHooks'
+import {AdaptiveMD3Theme} from '@models/ThemeInterface'
 import BackOfficeDrawer from '@navigation/BackOfficeDrawer'
+import CourseCreateScreen from '@screens/backoffice/CourseCreateScreen'
+import CourseDetailScreen from '@screens/backoffice/CourseDetailScreen'
 import CourseListScreen from '@screens/backoffice/CourseListScreen'
 import DashboardScreen from '@screens/backoffice/DashboardScreen'
 
@@ -18,29 +24,76 @@ export default function BackOfficeNavigator() {
     <Navigator
       key='BackOfficeNavigator'
       drawerContent={props => <BackOfficeDrawer {...props} />}
-      screenOptions={{
-        headerShown: false,
-        drawerStyle: {
-          backgroundColor:
-            responsive == 'MOBILE' ? 'transparent' : theme.colors.surface,
-          borderColor: theme.colors.outline,
-          width: responsive == 'MOBILE' ? width * 0.7 : width * 0.2,
-        },
-        drawerType: responsive == 'MOBILE' ? 'front' : 'permanent',
-        swipeEdgeWidth: width * 0.8,
-        swipeMinDistance: width * 0.1,
-      }}
+      screenOptions={screenOptions(theme, width, responsive)}
+      backBehavior='history'
       initialRouteName='Dashboard'>
-      <Screen name='Dashboard' component={DashboardScreen} />
-      <Screen name='CourseList' component={CourseListScreen} />
+      <Screen
+        name='Dashboard'
+        component={DashboardScreen}
+        options={{title: 'Dashboard'}}
+      />
+      <Screen
+        name='CourseList'
+        component={CourseListScreen}
+        options={{title: 'Courses'}}
+      />
+      <Screen
+        name='CourseCreate'
+        component={CourseCreateScreen}
+        options={{title: 'Course Create'}}
+      />
+      <Screen
+        name='CourseDetail'
+        component={CourseDetailScreen}
+        options={{title: 'Course Details'}}
+      />
     </Navigator>
   )
+}
+
+function screenOptions(
+  theme: AdaptiveMD3Theme,
+  width: number,
+  responsive: string,
+): DrawerNavigationOptions {
+  return {
+    headerShown: true,
+    headerLeft: responsive != 'MOBILE' ? () => null : undefined,
+    headerStyle: {
+      height: 70,
+      backgroundColor: theme.colors.surface,
+      borderBottomWidth: 1,
+      borderBottomColor: theme.colors.outlineVariant,
+    },
+    headerTintColor: theme.colors.onSurface,
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    headerTitleAlign: 'center',
+    drawerStyle: {
+      backgroundColor:
+        responsive == 'MOBILE' ? 'transparent' : theme.colors.surface,
+      borderColor: theme.colors.outline,
+      width: responsive == 'MOBILE' ? width * 0.7 : width * 0.2,
+    },
+    drawerType: responsive == 'MOBILE' ? 'front' : 'permanent',
+    overlayColor: 'transparent',
+    swipeEdgeWidth: width * 0.8,
+    swipeMinDistance: width * 0.1,
+  }
 }
 
 export type BackOfficeScreenParamList = {
   Dashboard: undefined
   CourseList: undefined
+  CourseCreate: undefined
+  CourseDetail: {courseId: number}
 }
 
 export type BackOfficeNavigatorParams =
   NavigatorScreenParams<BackOfficeScreenParamList>
+
+export type CourseDetailRouteProp = RouteProp<
+  BackOfficeScreenParamList,
+  'CourseDetail'
+>
