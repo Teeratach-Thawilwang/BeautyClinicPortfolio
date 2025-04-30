8855caa93c97cdaf0bdf72c0f87f68e538023acc
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-04-30 16:54:53 +0700

    Update props for SingleDateTimePicker components.

--- src/components/SingleDateTimePicker/types.ts
+++ src/components/SingleDateTimePicker/types.ts
@@ -8,4 +8,11 @@ export type Props = {
   height?: number
   backgroundColor?: string
   dividerColor?: string
+  modal?: boolean
+  title?: string | null
+  cancelText?: string
+  confirmText?: string
+  onCancel?: () => void
+  onConfirm?: (date: Date) => void
+  minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30
 }
