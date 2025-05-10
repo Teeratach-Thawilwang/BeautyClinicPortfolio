e117a3a10b23da84f4cf142a7684093f37465066
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-05-10 16:18:10 +0700

    Use new ButtonChoice and use DatePicker instead of SingleDateTimePicker for filtering created_at.

--- src/screens/backoffice/CourseListScreen/Components/Filter/styles.ts
+++ src/screens/backoffice/CourseListScreen/Components/Filter/styles.ts
@@ -7,12 +7,12 @@ export function getStyles(theme: AdaptiveMD3Theme) {
     container: {
       flexDirection: 'row',
       flexWrap: 'wrap',
-      gap: 20,
-      marginTop: 20,
+      gap: 10,
+      marginTop: 10,
     },
     createButtonContainer: {
       width: 110,
-      borderRadius: 10,
+      borderRadius: 5,
     },
     createButtonIcon: {
       width: 16,
@@ -22,7 +22,7 @@ export function getStyles(theme: AdaptiveMD3Theme) {
       marginLeft: 'auto',
       marginRight: 0,
       width: 100,
-      borderRadius: 10,
+      borderRadius: 5,
       borderColor: theme.colors.onSurfaceVariant,
     },
     filterButtonLabel: {
@@ -33,8 +33,7 @@ export function getStyles(theme: AdaptiveMD3Theme) {
       color: theme.colors.onSurface,
     },
     filterItemContainer: {
-      paddingVertical: 10,
-      borderBottomWidth: 1,
+      padding: 10,
       borderBottomColor: theme.colors.outlineVariant,
       gap: 10,
     },
@@ -42,11 +41,45 @@ export function getStyles(theme: AdaptiveMD3Theme) {
       color: theme.colors.onSurface,
       fontSize: theme.fontSize.label,
     },
-    bottomSheetSpaceBotton: {
-      height: 20,
+    buttomChoiceContainer: {
+      height: 36,
+      backgroundColor: theme.colors.surfaceContainerHighest,
     },
-    datePickerContainer: {
-      alignItems: 'center',
+    activeButtomChoiceContainer: {
+      height: 36,
+    },
+    bottomSheetHeader: {
+      padding: 10,
+      position: 'relative',
+    },
+    bottomSheetTitle: {
+      color: theme.colors.onSurface,
+      fontSize: theme.fontSize.subtitle,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    bottomSheetCloseIcon: {
+      width: 20,
+      position: 'absolute',
+      top: 0,
+      right: 10,
+    },
+    bottomSheetButtons: {
+      padding: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      gap: 10,
+      backgroundColor: theme.colors.surfaceContainerLow,
+    },
+    resetButtonContainer: {
+      backgroundColor: theme.colors.surfaceContainerHigh,
+      flexGrow: 1,
+    },
+    resetButtonLabel: {
+      color: theme.colors.onSurface,
+    },
+    confirmButtonContainer: {
+      flexGrow: 1,
     },
   })
 }
