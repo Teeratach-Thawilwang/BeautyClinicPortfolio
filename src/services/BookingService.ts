d6aaf9260e227ce69bc60d7dc1c942b91958d339
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-06-01 16:30:27 +0700

    Create booking service and hooks.

---
+++ src/services/BookingService.ts
@@ -0,0 +1,130 @@
+import dayjs from 'dayjs'
+
+import {BookingStatus} from '@enums/StatusEnums'
+import {
+  Booking,
+  BookingCreateProps,
+  BookingForm,
+  BookingList,
+  BookingListItem,
+  BookingUpdateProps,
+} from '@models/BookingTypes'
+import supabase from '@services/SupabaseClient'
+
+class BookingService {
+  private tableName = 'booking'
+
+  public async getList(
+    search?: string,
+    page: number = 1,
+    perPage: number = 15,
+    sortBy: string = 'id',
+    orderBy: 'ASC' | 'DESC' = 'DESC',
+    status?: BookingStatus,
+    startCreatedAt?: Date,
+    stopCreatedAt?: Date,
+  ): Promise<BookingList> {
+    const from = (page - 1) * perPage
+    const to = from + perPage - 1
+
+    let query = supabase
+      .from(this.tableName + '_view')
+      .select(
+        'id, \
+        user_id, \
+        course_id, \
+        booking_date, \
+        booking_time, \
+        status, \
+        created_at',
+        {count: 'exact'},
+      )
+      .order(sortBy, {ascending: orderBy == 'ASC'})
+
+    if (search) {
+      const orConditions: string[] = []
+      if (isNaN(Number(search))) {
+        orConditions.push(`user_id.ilike.%${search}%`)
+      } else {
+        orConditions.push(`id.eq.${search}`)
+        orConditions.push(`course_id.eq.${search}`)
+      }
+      query = query.or(orConditions.join(','))
+    }
+    if (status) {
+      query = query.eq('status', status)
+    }
+    if (startCreatedAt) {
+      query = query.gte('created_at', new Date(startCreatedAt).toISOString())
+    }
+    if (stopCreatedAt) {
+      query = query.lte('created_at', new Date(stopCreatedAt).toISOString())
+    }
+
+    const {data, error, count} = await query.range(from, to)
+    if (error) throw error
+
+    const transformData = data.map(item => {
+      const createdAt = dayjs(item.created_at).format('DD/MM/YYYY HH:mm')
+      const bookingDate = dayjs(item.booking_date).format('DD/MM/YYYY')
+      const bookingTime = `${item.booking_time.start} - ${item.booking_time.end}`
+
+      return {
+        id: item.id,
+        user_id: item.user_id,
+        course_id: item.course_id,
+        booking_date: bookingDate,
+        booking_time: bookingTime,
+        status: item.status,
+        created_at: createdAt,
+      }
+    })
+
+    return {
+      data: transformData as BookingListItem[],
+      last: count ? Math.ceil(count / perPage) : 1,
+    }
+  }
+
+  public async getById(id: number): Promise<BookingForm> {
+    const {data, error} = await supabase
+      .from(this.tableName)
+      .select('*')
+      .eq('id', id)
+      .single()
+
+    if (error) throw error
+
+    return {
+      ...data,
+      booking_date: dayjs(data.booking_date).format('DD/MM/YYYY'),
+      time_range: data.booking_time,
+    } as BookingForm
+  }
+
+  public async create(booking: BookingCreateProps): Promise<null> {
+    const {error} = await supabase.from(this.tableName).insert(booking)
+
+    if (error) throw error
+    return null
+  }
+
+  public async update(booking: BookingUpdateProps): Promise<null> {
+    const {id, ...updateParams} = {...booking}
+    const {error} = await supabase
+      .from(this.tableName)
+      .update(updateParams)
+      .eq('id', id)
+
+    if (error) throw error
+    return null
+  }
+
+  public async delete(id: number): Promise<null> {
+    const {error} = await supabase.from(this.tableName).delete().eq('id', id)
+    if (error) throw error
+    return null
+  }
+}
+
+export default new BookingService()
