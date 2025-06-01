8b8797b005b33fcafb692969456a354533d00f50
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-06-01 16:30:06 +0700

    Create booking type, enum.

---
+++ src/models/BookingTypes.ts
@@ -0,0 +1,45 @@
+import {TimeRange} from '@components/TimeRangePicker/types'
+import {BookingStatus} from '@enums/StatusEnums'
+
+export type Booking = {
+  id: number
+  user_id: string
+  course_id: number
+  booking_date: string
+  booking_time: TimeRange
+  status: BookingStatus
+  update_by: string
+  created_at: string
+  updated_at: string
+}
+
+export type BookingListItem = {
+  id: number
+  user_id: string
+  course_id: number
+  booking_date: string
+  booking_time: string
+  status: BookingStatus
+  created_at: string
+}
+
+export type BookingList = {
+  data: BookingListItem[]
+  last: number
+}
+
+export type BookingCreateProps = Omit<
+  Booking,
+  'id' | 'created_at' | 'updated_at'
+>
+
+export type BookingUpdateProps = Omit<Booking, 'created_at' | 'updated_at'>
+
+export type BookingForm = {
+  id: number
+  user_id: string
+  course_id: number
+  booking_date: string
+  booking_time: TimeRange
+  status: BookingStatus
+}
