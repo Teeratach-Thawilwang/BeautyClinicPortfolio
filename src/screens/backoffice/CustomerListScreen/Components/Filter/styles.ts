4205b6339000bd6674fd6f6a928f0452c2d2a696
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-06-13 07:24:37 +0700

    Create customer list screen and filter component.

---
+++ src/screens/backoffice/CustomerListScreen/Components/Filter/styles.ts
@@ -0,0 +1,80 @@
+import {StyleSheet} from 'react-native'
+
+import {AdaptiveMD3Theme} from '@models/ThemeTypes'
+
+export function getStyles(theme: AdaptiveMD3Theme) {
+  return StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      gap: 10,
+      marginTop: 10,
+    },
+    searchBar: {
+      width: '100%',
+    },
+    filterButtonContainer: {
+      marginLeft: 'auto',
+      marginRight: 0,
+      width: 100,
+      borderRadius: 5,
+      borderColor: theme.colors.onSurfaceVariant,
+    },
+    filterButtonLabel: {
+      color: theme.colors.onSurface,
+    },
+    filterButtonIcon: {
+      width: 20,
+      color: theme.colors.onSurface,
+    },
+    filterItemContainer: {
+      padding: 10,
+      borderBottomColor: theme.colors.outlineVariant,
+      gap: 10,
+    },
+    filterItemLabel: {
+      color: theme.colors.onSurface,
+      fontSize: theme.fontSize.label,
+    },
+    buttomChoiceContainer: {
+      height: 36,
+      backgroundColor: theme.colors.surfaceContainerHighest,
+    },
+    activeButtomChoiceContainer: {
+      height: 36,
+    },
+    bottomSheetHeader: {
+      padding: 10,
+      position: 'relative',
+    },
+    bottomSheetTitle: {
+      color: theme.colors.onSurface,
+      fontSize: theme.fontSize.subtitle,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    bottomSheetCloseIcon: {
+      width: 20,
+      position: 'absolute',
+      top: 0,
+      right: 10,
+    },
+    bottomSheetButtons: {
+      padding: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      gap: 10,
+      backgroundColor: theme.colors.surfaceContainerLow,
+    },
+    resetButtonContainer: {
+      backgroundColor: theme.colors.surfaceContainerHigh,
+      flexGrow: 1,
+    },
+    resetButtonLabel: {
+      color: theme.colors.onSurface,
+    },
+    confirmButtonContainer: {
+      flexGrow: 1,
+    },
+  })
+}
