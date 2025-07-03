35083a7185da4a5d9a2905cb2983066d0174a9af
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-07-04 05:17:36 +0700

    Install react-native-svg-transformer.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 messaging().setBackgroundMessageHandler(async (_remoteMessage) => { });
 
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
@@ -7,7 +10,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-    resetCache: true
+    resetCache: true,
+    transformer: {
+        babelTransformerPath: require.resolve(
+            "react-native-svg-transformer/react-native"
+        )
+    },
+    resolver: {
+        assetExts: assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...sourceExts, "svg"]
+    }
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
